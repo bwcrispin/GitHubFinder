8d6c9c4ddbb3ff04c47801062668908bc87661bc
Ben Crispin <benjamin.crispin@gmail.com>
2019-05-27 14:33:14 -0400

    updated files with working code

---
+++ app.js
@@ -0,0 +1,28 @@
+// Instantiate classes from other .js files
+const github = new GitHub;
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+  // Get input
+  const userText = e.target.value;
+  if(userText !== '') {
+    // Make HTTP call
+    github.getUser(userText)
+      .then(data => {
+        if(data.profile.message === 'Not Found') {
+          // Show alert
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          // Show profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+});
